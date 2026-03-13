fac0510274efea0ba18dfb824b7623b5d285e2d6
TienDat5604 <tiendat562004@gmail.com>
2026-03-13 14:16:16 +0700

    feat: Chatbot AI

--- fashionhub365_fe/src/components/Layout.jsx
+++ fashionhub365_fe/src/components/Layout.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { HeaderSection } from "./HeaderSection";
 import { FooterSection } from "./FooterSection";
 import Chatbot from "./Chatbot/Chatbot";
 
 export const Layout = ({ children }) => {
+  const location = useLocation();
+  const isAdminRoute = location.pathname.startsWith("/admin");
+
   return (
     <div className="App">
-      <HeaderSection />
+      {!isAdminRoute && <HeaderSection />}
       {children}
-      <FooterSection />
-      <Chatbot />
+      {!isAdminRoute && <FooterSection />}
+      {!isAdminRoute && <Chatbot />}
     </div>
   );
 };
